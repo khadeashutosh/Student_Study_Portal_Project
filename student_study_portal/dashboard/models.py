027a56436ecac418316e2921e90d7fd0aca3e1c1
Ashu <ashutoshkhade2530@gmail.com>
2025-01-27 10:25:45 +0530

    Completed Student Study Portal and ready to deployement

--- student_study_portal/dashboard/models.py
+++ student_study_portal/dashboard/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import user
 
 # Create your models here.
+class Nodes(models.Model):
+  user= models.ForeignKey(user,on_delete=models.CASCADE)
