e726cf599605fc92f43dd33905b603dee02b7dd4
Ashu <ashutoshkhade2530@gmail.com>
2025-05-26 17:50:01 +0530

    Completed Student Study Portal Project ready to run and deployement

--- student_study_portal/dashboard/urls.py
+++ student_study_portal/dashboard/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
 
-    path('notes',views.notes,name='notes'),
+    path('notes/',views.notes,name='notes'),
     path('delete_notes/<int:pk>',views.delete_notes,name='delete_notes'),
     path('notes_detail/<int:pk>',views.Notes_Detail_View.as_view(),name='notes_detail'),
 
